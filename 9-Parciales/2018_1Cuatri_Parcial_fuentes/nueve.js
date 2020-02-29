803e53c37e21ad3fe6133af43379721cea339a58
alumno <alumno>
2020-02-28 22:17:07 -0300

    finalday

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,204 @@
 function mostrar()
 {
 
+var peso;
+var cant= 0;
+var temperatura;
+var marca;
+var tempPares= 0;
+var marcaPesado = 0;
+var bajoCeroCont= 0;
+var acumuladorPeso=0;
+var pesoMax;
+var pesoMin;
+var flag = 0;
+var seguir;
+var promedio;
+do
+{
+
+    marca = prompt("Ingrese la Marca: ");
+
+    peso = parseInt(prompt("Ingrese Peso: 1 al 100"));
+    
+    while   ( peso < 1 || peso >100 )
+    {
+    peso = parseInt(prompt("Error: Ingrese Peso: 1 al 100"));
+    }
+    
+    temperatura = parseInt(prompt("Ingrese la Temperatura: -30 a 30"));
+    
+    while ( temperatura < -30 || temperatura > 30)
+    {
+        
+        temperatura = parseInt(prompt("Error :Ingrese la Temperatura: -30 a 30"));
+    }
+    
+    cant++;
+    acumuladorPeso = acumuladorPeso + peso;
+
+    if ( temperatura %  2 == 0)
+    {
+            tempPares++;
+    }
+
+    if ( peso > marcaPesado)
+    {
+        marcaPesado = peso;
+        marcaPesado = marca;
+    }
+
+    if ( temperatura < 0)
+    {
+        bajoCeroCont++;
+    }
+
+    if (peso > pesoMax || flag == 0)
+    {
+        pesoMax = peso;
+    }
+    
+    if (peso < pesoMax || flag == 0)
+    {
+        pesoMin= peso;
+        flag++;
+    }
+
+    seguir = prompt(" Quiere continuar. s o n");
+
+    while (!(seguir == "n" || seguir == "s")) {
+                
+        seguir = prompt("Quiere continuar: s o n ");
+    }
+
+}while(seguir == "s");
+
+
+
+    promedio = acumuladorPeso / cant;
+
+
+document.write("a) La cantidad de temperaturas pares: " + tempPares + "</br>" ); 
+document.write("b) La marca del producto más pesado: " + marcaPesado + "</br>" );
+document.write("c) La cantidad de productos que se conservan a menos de 0 grados: " +bajoCeroCont +  "</br>");
+document.write("d) El promedio del peso de todos los productos: " + promedio + "</br>");
+document.write("f) El peso máximo: " + pesoMax + "</br>");
+document.write("f) El peso mínimo: " + pesoMin + "</br>");
+document.write( acumuladorPeso);
+
+
 }
+
+
+/*
+
+Bienvenidos. 
+Realizar el algoritmo que permita ingresar la marca del producto, el peso el cual debe ser
+ entre 1 y 100 (validar), 
+la temperatura de almacenamiento(entre -30 y 30) hasta que el usuario quiera e informar 
+al terminar el ingreso por document.write: 
+
+function mostrar() {
+    var temperatura;
+    var marca;
+    var peso;
+    var contadorPares = 0;
+    var contadorCong = 0;
+    var marcaPesada;
+    var pesoMaximoMas0;
+    var pesoMaximo;
+    var pesoMinimo;
+    var promedioPeso;
+    var acumuladorPeso = 0;
+    var contador = 0;
+    var seguir;
+    var flag = 0;
+
+    //--------------------------------------------
+
+    do {
+        marca = prompt("Ingrese marca: ");
+
+        peso = parseInt(prompt("Ingrese un peso (1 y 100): "));
+        while (peso < 1 || peso > 100 || isNaN(peso)) {
+            peso = parseInt(prompt("Peso invalido. Ingrese un peso (1 y 100): "));
+        }
+        temperatura = parseInt(prompt("Ingrese un temperatura (-30 y 30): "));
+        while (temperatura < -30 || temperatura > 30 || isNaN(temperatura)) {
+            temperatura = parseInt(prompt("Temperatura invalida. Ingrese un temperatura (-30 y 30): "));
+        }
+
+        if (temperatura % 2 == 0) {
+            contadorPares++;
+        }
+
+        if ((flag == 0 || peso > pesoMaximoMas0) && temperatura > 0) {
+            pesoMaximoMas0 = peso;
+            marcaPesada = marca;
+            flag = 1;
+        }
+
+        if (temperatura < 0) {
+            contadorCong++;
+        }
+
+        acumuladorPeso = acumuladorPeso + peso;
+
+        if (contador == 0 || peso > pesoMaximo) {
+            pesoMaximo = peso;
+        }
+        if (contador == 0 || peso < pesoMinimo) {
+            pesoMinimo = peso;
+        }
+
+        contador++;
+
+        seguir = prompt("Quiere continuar?:");
+    } while (seguir == 's');
+
+    promedioPeso = acumuladorPeso / contador;
+
+    if( flag == 0){
+        marcaPesada = "sin datos";
+    }
+
+    document.write("a) La cantidad de temperaturas pares: " + contadorPares + "</br>");
+    document.write("b) La marca del producto más pesado no congelado: " + marcaPesada + "</br>");
+    document.write("c) La cantidad de productos que se conservan a menos de 0 grados: " + contadorCong + "</br>");
+    document.write("d) El promedio del peso de todos los productos: " + promedioPeso + "</br>");
+    document.write("f) El peso máximo: " + pesoMaximo + " y el mínimo: " + pesoMinimo + "</br>");
+
+
+}
+
+        // ------------- Maximos y minimos ---------------
+        if (flag == 0 || numero < numeroMin) {
+            numeroMin = numero;
+            letraMin = letra;
+        }
+        if (flag == 0 || numero > numeroMax) {
+            numeroMax = numero;
+            letraMax = letra;
+            flag = 1;
+        }
+
+        seguir = prompt("Quiere continuar?:");
+    } while (seguir == 's');
+
+    if (contadorPos != 0) {
+        promedioPos = acumuladorPos / contadorPos;
+    }
+
+    document.write("a) Cantidad de números pares: " + contadorPares + "</br>");
+    document.write("b) Cantidad de números impares: " + contadorImpares + "</br>");
+    document.write("c) Cantidad de ceros: " + contadorCeros + "</br>");
+    document.write("d) Promedio de todos los números positivos ingresados: " + promedioPos.toFixed(2) + "</br>");
+    document.write("e) La suma de todos los números negativos: " + acumuladorNeg + "</br>");
+    document.write("f) Número Máximo: " + numeroMax + " Letra: " + letraMax + "</br>");
+    document.write("f) Número Minimo: " + numeroMin + " Letra: " + letraMin + "</br>");
+
+}
+
+
+
+*/
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,203 @@
-function mostrar()
+function mostrar() 
 {
 
+
+    var numero=0;
+    var acumuladorPositivo=0;
+    var acumuladorNegativo=0;
+    var contPositivo=0;
+    var contNegativo=0;
+    var contCero=0;
+    var maximo=0;
+    var letraMaximo;
+    var minimo=0;
+    var letraMinimo;
+    var letra;
+    var contPares=0;
+    var contImpar=0;
+    var seguir;
+    var flag = 0;
+    do    
+    {
+        
+        
+        letra = prompt("Ingrese una letra: ");
+        
+        while (!((letra >= "A" && letra <= "Z") || (letra >= "a" && letra <= "z"))) 
+        {
+            letra = prompt(letra+ " no es una letra. Ingrese una letra: ");
+        }
+        
+        numero = parseInt(prompt("Ingrese nota(-100 al 100): "));
+
+        while (numero < -100 || numero > 100 || isNaN(numero)) 
+        {
+            numero = parseInt(prompt("Numero invalido. Ingrese numero(-100 al 100): "));
+        }
+        
+        
+        if (numero > 0) 
+        {
+            acumuladorPositivos = acumuladorPositivo + numero;
+            contPositivo++
+            
+        }
+        
+        else if(numero < 0) 
+        {
+            
+            acumuladorNegativo = acumuladorNegativo + numero;
+            
+        }
+        
+        else
+        {
+            contCero++;
+        }
+        
+        if(flag == 0 || numero < minimo)
+        {
+            minimo = numero;
+            letraMinimo = letra;
+            
+        }
+        
+        if(flag == 0 || numero >  maximo)
+        {
+            maximo = numero;
+            letraMaximo = letra;
+            flag++;
+        }
+        
+        if( numero % 2 == 0)
+        {
+                contPares++;
+            }
+            
+            if( numero % 2 != 0)
+            {
+                contImpar++;
+            }
+            
+            seguir = prompt("Quiere continuar: s o n ");
+
+            
+            while (!(seguir == "n" || seguir == "s")) {
+                
+                seguir = prompt("Quiere continuar: s o n ");
+            }
+
+            
+    }while( seguir == "s");
+
+    document.write("La Cantidad de Numeros pares es: " + contPares + "</br>");
+    document.write("La Cantidad de Numeros impares es: " + contImpar + "</br>");
+    document.write("La Cantidad de Ceros es: " + contCero + "</br>");
+    document.write("El promedio de todos los números positivos ingresados: " + (acumuladorPositivo / contPositivo) + "</br>");
+    document.write("La suma de todos los números negativos: " + acumuladorNegativo + "</br>");
+    document.write("El Numero Maximo es: " + maximo + " y su letra es: " + letraMaximo + "</br>");
+    document.write("El Numero Minimo es: " + minimo + " y su letra es: " + letraMinimo + "</br>");
+
+
+
+        
+        
+        
+      
+    
+
 }
+
+/*a) La cantidad de números pares. 
+b) La cantidad de números impares. 
+c) La cantidad de ceros. 
+d) El promedio de todos los números positivos ingresados. 
+e) La suma de todos los números negativos. 
+f) El número y la letra del máximo y el mínimo.
+
+function mostrar() {
+    var letra;
+    var numero;
+    var seguir;
+    var contadorPares = 0;
+    var contadorImpares = 0;
+    var contadorCeros = 0;
+    var acumuladorPos = 0;
+    var contadorPos = 0;
+    var promedioPos = 0;
+    var acumuladorNeg = 0;
+    var numeroMax;
+    var numeroMin;
+    var letraMax;
+    var letraMin;
+    var flag = 0;
+
+
+    do {
+        letra = prompt("Ingrese una letra: ");
+        while (!((letra >= 'A' && letra <= 'Z') || (letra >= 'a' && letra <= 'z'))) {
+            letra = prompt("Eso no es una letra. Ingrese una letra: ");
+        }
+
+        numero = parseInt(prompt("Ingrese un numero (-100 y 100): "));
+        while (numero < -100 || numero > 100 || isNaN(numero)) {
+            numero = parseInt(prompt("Numero invalido. Ingrese un numero (-100 y 100): "));
+        }
+
+        // ---------Pares-----------
+        if (numero % 2 == 0) {
+            // par
+            contadorPares++;
+        }
+        else {
+            // impar
+            contadorImpares++;
+        }
+
+        // ------------- Signo------------------
+
+        if (numero > 0) {
+            // positivos
+            acumuladorPos = acumuladorPos + numero;
+            contadorPos++;
+        }
+        else if (numero < 0) {
+            // negativos
+            acumuladorNeg = acumuladorNeg + numero;
+        }
+        else {
+            // cero
+            contadorCeros++;
+        }
+
+        // ------------- Maximos y minimos ---------------
+        if (flag == 0 || numero < numeroMin) {
+            numeroMin = numero;
+            letraMin = letra;
+        }
+        if (flag == 0 || numero > numeroMax) {
+            numeroMax = numero;
+            letraMax = letra;
+            flag = 1;
+        }
+
+        seguir = prompt("Quiere continuar?:");
+    } while (seguir == 's');
+
+    if (contadorPos != 0) {
+        promedioPos = acumuladorPos / contadorPos;
+    }
+
+    document.write("a) Cantidad de números pares: " + contadorPares + "</br>");
+    document.write("b) Cantidad de números impares: " + contadorImpares + "</br>");
+    document.write("c) Cantidad de ceros: " + contadorCeros + "</br>");
+    document.write("d) Promedio de todos los números positivos ingresados: " + promedioPos.toFixed(2) + "</br>");
+    document.write("e) La suma de todos los números negativos: " + acumuladorNeg + "</br>");
+    document.write("f) Número Máximo: " + numeroMax + " Letra: " + letraMax + "</br>");
+    document.write("f) Número Minimo: " + numeroMin + " Letra: " + letraMin + "</br>");
+
+}
+
+
+
+*/
